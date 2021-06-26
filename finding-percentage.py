7b146da854d8b62e797f9b473bbb3e37529bc3e4
David <davidsantander9@gmail.com>
2021-06-26 14:32:18 -0500

    Add findin percentage problem

---
+++ finding-percentage.py
@@ -0,0 +1,18 @@
+# Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    sum = 0
+    for score in student_marks[query_name]:
+        sum += score
+
+    average = sum / len(student_marks[query_name])
+    format_float = "{:.2f}".format(average)
+    print(average)
